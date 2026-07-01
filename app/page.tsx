ccdff29da9bbad301f5ad32f4123b962602c2d68
Leandro pinheiro <leandropinheiro478@gmail.com>
2026-07-01 01:09:18 -0300

    chore:add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-"use client";
-
-import { useState } from "react";
-
-//SERVER COMPONENTS > Uma Possibilidade de acessar o  banco de dados, mas não interatividade
 const Home = () => {
-  const [] = useState();
-  return <h1 className="text-red-400 mb-5 bg-red-400">red text</h1>;
+  return <button>test</button>;
 };
 
 export default Home;
